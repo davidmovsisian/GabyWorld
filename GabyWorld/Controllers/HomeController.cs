e5ff509abfce16ae527b01e5c6f5f536de184cd2
my24101974 <my24101974@yahoo.com>
2025-05-13 14:00:43 +0300

    Add cookie authentication

--- GabyWorld/Controllers/HomeController.cs
+++ GabyWorld/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using GabyWorld.Data;
 using GabyWorld.IOC;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GabyWorld.Controllers
@@ -8,10 +10,18 @@ namespace GabyWorld.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public HomeController(ILogger<HomeController> logger, 
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
         {
             _logger = logger;
             _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
@@ -35,6 +45,58 @@ namespace GabyWorld.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        [Route("create")]
+        public async Task<IActionResult> Create()
+        {
+            var result = await _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = "DavidM",
+                Email = "davidm@gmail.com"
+            }, "password");
+
+            if(result.Succeeded) 
+                return Content("User was created", "text/html");
+
+            return Content("User creation failed", "text/html");
+        }
+
+        [Authorize]
+        [Route("private")]
+        public IActionResult Private()
+        {
+            return Content($"Welcome to the private area {HttpContext.User.Identity.Name}");
+        }
+
+        [Route("login")]
+        public async Task<IActionResult> LogInAsync(string returnUrl)
+        {
+            //sign out previous session
+            await _signInManager.SignOutAsync();
+
+            //try to log in
+            var result = await _signInManager.PasswordSignInAsync("DavidM", "password", true, false);
+            //if login succeedd
+            if(result.Succeeded)
+            {
+                //if we 
+                if (string.IsNullOrEmpty(returnUrl))
+                    return RedirectToAction(nameof(Index));
+                
+                return Redirect(returnUrl);
+            }
+
+            return Content("Failed to log in", "text/html");
+
+        }
+
+        [Route("logout")]
+        public async Task<IActionResult> LogOutAsync()
+        {
+            await _signInManager.SignOutAsync();
+
+            return Content("done");
         }
     }
 }
--- GabyWorld/Startup.cs
+++ GabyWorld/Startup.cs
@@ -1,5 +1,7 @@
-﻿using GabyWorld.Data;
+﻿using System;
+using GabyWorld.Data;
 using GabyWorld.IOC;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,6 +27,46 @@ namespace GabyWorld
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            #region Add Authentication services
+            // AddIdentity adds cookie base authentication.
+            // Adds scoped classes for things like UserManager, SignInManager, PasswordHashers etc..
+            // NOTE: Automatically adds the validated user from a cookie to the HttpContext.User
+            // https://github.com/aspnet/Identity/blob/85f8a49aef68bf9763cd9854ce1dd4a26a7c5d3c/src/Identity/IdentityServiceCollectionExtensions.cs
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                //add UserStore and RoleStore from this context.
+                //that are consumed by UserManager and RoleManager
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                // Adds a provider that generates unique keys and hashes for things like
+                // forgot password links, phone number verification codes etc...
+                .AddDefaultTokenProviders();
+
+            //add code to enable weak password creation, to pass validation 
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+            });
+
+            //alter application cookie info
+            services.ConfigureApplicationCookie(options =>
+            {
+                //change login url to "hostname/login". for example if user access to private area he will be redirected to login page
+                options.LoginPath = "/login";
+
+                //set cookiw timeout period
+                options.ExpireTimeSpan = TimeSpan.FromSeconds(15);
+            });
+
+            services.Configure<SecurityStampValidatorOptions>(options =>
+            {
+                options.ValidationInterval = TimeSpan.FromMinutes(30);
+            });
+            #endregion
+
+
+
             services.AddMvc();
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
@@ -32,6 +74,8 @@ namespace GabyWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IWebHostEnvironment env)
         {
+            app.UseAuthentication();//the authentication midleware used services added in ConfigureServices.
+
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
             else
